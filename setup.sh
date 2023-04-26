7aa7f267a35a0eab971c08827ca65560c7530425
ajilty <github@ajilty.com>
2023-04-25 21:11:15 -0400

    gh codespace support

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Do not rename this file. It is sourced by the GitHub Codespaces to install dotfiles
+# https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-github-codespaces-for-your-account#dotfiles
+
+sh ./bin/setup-dotfiles.sh
