b0e96ea7b3bdd96ff838f8c7a946a9e83103a17d
ajilty <github@ajilty.com>
2026-03-12 15:35:33 -0400

    no more acli

--- .config/shell/profile.d/acli.sh
+++
@@ -1,8 +0,0 @@
-# Atlassian CLI
-
-# if acli command is installed and available in the PATH, then enable autocompletion for zsh
-#  eval "$(acli completion zsh)"
-if command -v acli &> /dev/null; then
-  eval "$(acli completion zsh)"
-fi
-
